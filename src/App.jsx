d69b2ad9521782f073d9696f3a0400d9e2244894
Ifxscripts <ifxscripts@Ifxscriptss-Mac-mini.local>
2026-04-06 08:36:09 +0100

    Add ServicesTeaser component with rounded cards and editorial style

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* We can add /about, /services, /gallery here later */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,15 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import ServicesTeaser from '../components/ServicesTeaser';
 
 function LandingPage() {
   return (
     <div className="landing-page">
       <Navbar />
-      <Hero />
+      <div id="home">
+        <Hero />
+      </div>
+      <ServicesTeaser />
     </div>
   );
 }
